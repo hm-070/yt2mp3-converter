90e7d8733707ac69071f0f84d6285ce07864fef9
hm07 <harrytmacro@gmail.com>
2026-07-01 20:57:13 +0100

    gui is working great - check readme for todo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # yt2mp3-converter
 A personal project to learn new skills 
+
+TODO:
+- Add some function to fill out a form or something to add metadata to a file
+- Get some way to download the thumbnail of a song and make it so (metadata?)
+- test and QOL
--- main.py
+++ main.py
@@ -7,12 +7,32 @@ import threading
 progress_value = 0
 
 def start_download():
-    threading.Thread(target=downloadAs_m4a, daemon=True).start()
+    want_m4a = app.optionsFrame.m4acheckBox.get()
+    want_mp4 = app.optionsFrame.mp4checkBox.get()
+    if want_m4a == 1:
+        threading.Thread(target=downloadAs_m4a, daemon=True).start()
+    elif want_mp4 == 1:
+        threading.Thread(target=downloadAs_MP4, daemon=True).start()
+
+def downloadAs_MP4():
+    global progress_value
+    progress_value = 0
+    inp = app.downloadFrame.urlEntry.get()
+    options = {
+        "format": "best",
+        "outtmpl": "%(title)s.%(ext)s",
+        "progress_hooks": [progress_hook],
+    }
+    app.statusFrame.statusLabel.configure(text="Downloading...")
+    with YoutubeDL(options) as ydl:
+        error_code = ydl.download([inp])
+        print(error_code)
+    app.statusFrame.statusLabel.configure(text="Finished Downloading")
 
 def downloadAs_m4a():
     global progress_value
     progress_value = 0
-    inp = urlEntry.get()
+    inp = app.downloadFrame.urlEntry.get()
     options = {
         "format": "m4a/bestaudio/best",
         "outtmpl": "%(title)s.%(ext)s",
@@ -22,11 +42,11 @@ def downloadAs_m4a():
             'preferredcodec': 'm4a',
         }]
     }
-    statusLabel.configure(text="Downloading...")
+    app.statusFrame.statusLabel.configure(text="Downloading...")
     with YoutubeDL(options) as ydl:
         error_code = ydl.download([inp])
         print(error_code)
-    statusLabel.configure(text="Finished Downloading")
+    app.statusFrame.statusLabel.configure(text="Finished Downloading")
 
 def progress_hook(d):
     global progress_value
@@ -38,23 +58,74 @@ def progress_hook(d):
             progress_value = downloaded/total
 
 def update_progBar():
-    progressbar.set(progress_value)
+    app.statusFrame.progressbar.set(progress_value)
     app.after(100, update_progBar)
 
-app = ctk.CTk()
-app.grid_columnconfigure(0, weight=1)
-app.grid_columnconfigure(2, weight=1)
-app.geometry("400x400")
-title = ctk.CTkLabel(app, text="YouTube to MP3 Downloader")
-button = ctk.CTkButton(app, text="Download", command=start_download)
-urlEntry = ctk.CTkEntry(app, placeholder_text="Paste YouTube URL Here", width=400)
-progressbar = ctk.CTkProgressBar(app, width=350)
-statusLabel = ctk.CTkLabel(app ,text="Idle")
-progressbar.set(0)
-statusLabel.grid(row=3, column=1, padx=20, pady=20)
-progressbar.grid(row=4, column=1, padx=20, pady=20, sticky="ew")
-title.grid(row=0, column=1, padx=20, pady=20)
-urlEntry.grid(row=1 ,column=0, padx=20, pady=20, sticky="ew", columnspan=3)
-button.grid(row=2, column=1, padx=20, pady=20)
+class top_frame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.title = ctk.CTkLabel(self, text="YouTube to MP3 Downloader")
+        self.title.pack(padx=20)
+
+class downloadFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.urlEntry = ctk.CTkEntry(self, placeholder_text="Paste YouTube URL Here", width=400)
+        self.button = ctk.CTkButton(self, text="Download", command=start_download)
+        self.urlEntry.grid(row=0 ,column=0, padx=20, pady=20, sticky="ew", columnspan=3)
+        self.button.grid(row=1, column=1, padx=20, pady=20)
+
+class statusFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.progressbar = ctk.CTkProgressBar(self, width=350)
+        self.progressbar.set(0)
+        self.statusLabel = ctk.CTkLabel(self ,text="Idle")
+        self.statusLabel.pack(padx=20, pady=20)
+        self.progressbar.pack(padx=20, pady=20)
+
+class optionsFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+
+        self.m4acheckBox = ctk.CTkCheckBox(self, text="m4a")
+        self.mp4checkBox = ctk.CTkCheckBox(self, text="MP4")
+        self.warningLabel = ctk.CTkLabel(self, text="Tick only ONE option: ")
+
+        self.warningLabel.grid(row=0, column=1, padx=20, pady=20)
+        self.mp4checkBox.grid(row=1, column=2, padx=20, pady=20)
+        self.m4acheckBox.grid(row=1, column=0, padx=20, pady=20)
+
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+
+        self.geometry("800x600")
+
+        self.top_frame = top_frame(master=self)
+        self.top_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew", columnspan=4)
+
+        self.downloadFrame = downloadFrame(master=self)
+        self.downloadFrame.grid(row=1, column=0, padx=20, pady=20, sticky="nsew", columnspan=4)
+
+        self.statusFrame = statusFrame(master=self)
+        self.statusFrame.grid(row=2, column=0, padx=20, pady=20, sticky="nsew", columnspan=4)
+
+        self.optionsFrame = optionsFrame(master=self)
+        self.optionsFrame.grid(row=3, column=0, padx=20, pady=20, sticky="nsew", columnspan=4)
+
+app = App()
 update_progBar()
 app.mainloop()
