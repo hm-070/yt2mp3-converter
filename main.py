1f6d05174b0e04e921da2d2006d86174c3c934d7
hm07 <harrytmacro@gmail.com>
2026-07-01 19:54:19 +0100

    got gui working yippee

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
 import re
 from yt_dlp import YoutubeDL
+import customtkinter as ctk
+import threading
 
+#download progress global value
+progress_value = 0
 
-def main():
-    url = get_vidID_from_user_inp()
+def start_download():
+    threading.Thread(target=downloadAs_m4a, daemon=True).start()
 
-def get_vidID_from_user_inp():
-    inp = input("Enter a Youtube URL: ")
-    match = re.search(r'youtube\.com\/watch\?v=', inp)
-    if match:
-        print("Video ID: ", inp[match.end():])
-        downloadAs_m4a(inp)
-    else:
-        print("No match")
-        get_vidID_from_user_inp()
-    return inp
-
-def yt_dlpTest(inp):
-    print("URL:", inp)
-    options = {
-        "format": "best",
-        "outtmpl": "%(title)s.%(ext)s",
-    }
-    print("Creating YoutubeDL...")
-    with YoutubeDL(options) as ydl:
-        print("Starting download...")
-        ydl.download([inp])
-    print("Finished!")
-
-def downloadAs_m4a(inp):
-    print("URL:", inp)
+def downloadAs_m4a():
+    global progress_value
+    progress_value = 0
+    inp = urlEntry.get()
     options = {
         "format": "m4a/bestaudio/best",
         "outtmpl": "%(title)s.%(ext)s",
+        "progress_hooks": [progress_hook],
         "postprocessors": [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'm4a',
         }]
     }
-    print("Creating YoutubeDL...")
+    statusLabel.configure(text="Downloading...")
     with YoutubeDL(options) as ydl:
-        print("Starting download...")
         error_code = ydl.download([inp])
         print(error_code)
-    print("Finished!")
+    statusLabel.configure(text="Finished Downloading")
+
+def progress_hook(d):
+    global progress_value
+
+    if d["status"] == "downloading":
+        downloaded = d.get("downloaded_bytes", 0)
+        total = d.get("total_bytes") or d.get("total_bytes_estimate")
+        if total:
+            progress_value = downloaded/total
+
+def update_progBar():
+    progressbar.set(progress_value)
+    app.after(100, update_progBar)
+
+app = ctk.CTk()
+app.grid_columnconfigure(0, weight=1)
+app.grid_columnconfigure(2, weight=1)
+app.geometry("400x400")
+title = ctk.CTkLabel(app, text="YouTube to MP3 Downloader")
+button = ctk.CTkButton(app, text="Download", command=start_download)
+urlEntry = ctk.CTkEntry(app, placeholder_text="Paste YouTube URL Here", width=400)
+progressbar = ctk.CTkProgressBar(app, width=350)
+statusLabel = ctk.CTkLabel(app ,text="Idle")
+progressbar.set(0)
+statusLabel.grid(row=3, column=1, padx=20, pady=20)
+progressbar.grid(row=4, column=1, padx=20, pady=20, sticky="ew")
+title.grid(row=0, column=1, padx=20, pady=20)
+urlEntry.grid(row=1 ,column=0, padx=20, pady=20, sticky="ew", columnspan=3)
+button.grid(row=2, column=1, padx=20, pady=20)
+update_progBar()
+app.mainloop()
